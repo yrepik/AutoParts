3b149abfefec9dd6be5bdffa567f9890f6b856fe
Podovzhniy <podovzhniy@gmail.com>
2016-11-25 16:17:29 +0200

    Issue #54 fix

--- AutoPartsWebSite/Controllers/AccountController.cs
+++ AutoPartsWebSite/Controllers/AccountController.cs
@@ -71,11 +71,15 @@ namespace IdentityAutoPart.Controllers
                 return View(model);
             }
 
-            var userid = UserManager.FindByEmail(model.Email).Id;
-            if (!UserManager.IsEmailConfirmed(userid))
+            if (UserManager.FindByEmail(model.Email) != null)
             {
-                return View("EmailNotConfirmed");
+                var userid = UserManager.FindByEmail(model.Email).Id;
+                if (!UserManager.IsEmailConfirmed(userid))
+                {
+                    return View("EmailNotConfirmed");
+                }
             }
+            
             // This doen't count login failures towards lockout only two factor authentication
             // To enable password failures to trigger lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
